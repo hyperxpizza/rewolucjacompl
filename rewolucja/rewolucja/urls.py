3d7bef2a77385fbcb9fd367603239876aa8f2f52
hyperxpizza <wojtek.frackowski@gmail.com>
2020-11-16 20:15:17 +0100

    update url for media files

--- rewolucja/rewolucja/urls.py
+++ rewolucja/rewolucja/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.sitemaps.views import sitemap
+from django.conf.urls.static import static
+from django.conf import settings
 from django.views.generic.base import TemplateView
 
 from website.sitemaps import Static_Sitemap, Post_Sitemap, Product_Sitemap
@@ -19,3 +21,5 @@ urlpatterns = [
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
